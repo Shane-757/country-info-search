b91f3ac747bc9d26d80d35377397bcbda01740eb
Shane-757 <underwoodshane@ymail.com>
2023-03-01 16:50:41 -0500

    update

--- src/index.js
+++ src/index.js
@@ -5,24 +5,31 @@ import { fetchCountries } from './fetchCountries';
 
 const DEBOUNCE_DELAY = 300;
 const input = document.querySelector('input');
-
+const countryList = document.querySelector('.country-list');
+const countryInfo = document.querySelector('.country-info');
 
 input.addEventListener('input', debounce((e) => {
-    countryInput = e.target.value.trim;
-    if (data.length === 0) {
-        return;
-    }
-
+    const countryInput = e.target.value.trim();
+    if (countryInput === ""){
+        return
+    };
     fetchCountries(countryInput)
-    .then(data => {
-        if (data.length > 10) {
-            Notiflix.Notify.info("Too many matches found. Please enter a more specific name.");
-        } else if (data.length >= 2 && data.length <= 10) {
-            console.log(data)
-        } else if (data.length === 1) {
-            console.log(data)
-        } 
-        // Data handling
+        .then(data => {
+            countryInfo.innerHTML = "";
+            countryList.innerHTML = "";
+            if (data.length > 10) {
+                Notiflix.Notify.info("Too many matches found. Please enter a more specific name.");
+            } else if (data.length >= 2 && data.length <= 10) {
+                countryList.innerHTML = countryListMarkup(data);
+                console.log(data)
+            } else if (data.length === 1) {
+                countryInfo.innerHTML = countryMarkup(data);
+                console.log(data)
+            } else if (data.length === 0) {
+                input.value = "";
+                return;
+            }
+            // Data handling
         })
         .catch(error => {
                 Notiflix.Notify.failure("Oops, there is no country with that name")
@@ -31,9 +38,32 @@ input.addEventListener('input', debounce((e) => {
 }, DEBOUNCE_DELAY)
 );
 
+function countryListMarkup(data) {
+   return data.map(country => 
+    `<li>
+    <img
+    src = "${country.flags.svg}"
+    alt = "${country.name} flag" 
+    width = 25px
+    </img>
+    <h2>${country.name.official}</h2>
+    </li>`
+    );
+    
+}
 
-
-
-
-
-
+function countryMarkup(data) {
+    return data.map(country =>
+    `<ul>
+    <li><img
+    src = "${country.flags.svg}"
+    alt = "${country.name} flag" 
+    width = 25px
+    </img></li>
+    <li><h2>${country.name.official}</h2></li>
+    <li>${country.capital}</li>
+    <li>${country.population}</li>
+    <li>${Object.values(country.languages)}</li>
+    </ul>`
+    );
+}
